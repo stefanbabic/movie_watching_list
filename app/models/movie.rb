bde2909f7e7ac3c69b6f1802d6c885f4f0bc71f2
Stefan Babic <babic.stefan@gmail.com>
2016-08-26 15:09:26 +0200

    add finished and recent to Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,8 @@
 class Movie < ApplicationRecord
 
+  scope :finished, ->{ where.not(finished_on: nil) }
+  scope :recent, ->{ where('finished_on > ?', 10.days.ago) }
+
   def finished?
     finished_on.present?
   end

9f86fe1b9922e9a515fdbf301ba02b3ffec01fe6
Stefan Babic <babic.stefan@gmail.com>
2016-09-07 11:30:07 +0200

    add basic search function

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
 
   scope :finished, ->{ where.not(finished_on: nil) }
   scope :recent, ->{ where('finished_on > ?', 10.days.ago) }
+  scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
 
   def finished?
     finished_on.present?
